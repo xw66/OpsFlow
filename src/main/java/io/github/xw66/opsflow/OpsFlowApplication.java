e9343e15883890d13f7aca8418c3b4a46d910083
xw <2787155375xw@gmail.com>
2026-09-15 19:48:17 +0800

    后端业务、数据与测试：启动后端应用

---
+++ src/main/java/io/github/xw66/opsflow/OpsFlowApplication.java
@@ -0,0 +1,13 @@
+package io.github.xw66.opsflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpsFlowApplication.class, args);
+    }
+
+}
