aac4e50860b09c09ae029ad22607ebae8a48a0dd
xw <2787155375xw@gmail.com>
2026-09-15 19:48:22 +0800

    后端业务、数据与测试：配置定时任务执行与时钟

---
+++ src/main/java/io/github/xw66/opsflow/config/SchedulingConfig.java
@@ -0,0 +1,8 @@
+package io.github.xw66.opsflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig { }
